4d056ebbbb86562c2260b485c691d236b7487d70
Andrew Jyan <andrew.jyan4@gmail.com>
2020-03-24 19:32:44 -0400

    added card from react-bootstrap

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,27 +1,30 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import Carousel from 'react-bootstrap/Carousel'
 
 export default class RelatedProducts extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      relatedProducts: []
+    };
   }
 
   render() {
     return (
-    <div>
-        <div class='container'>
-          <div class='col-sm-5'>
-            <div class="card text-left 2-50">
-            <img src='https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4'></img>
-            <div class="card-body"></div>
-              <div>Category</div>
-              <h5 class="card-title">Product Name</h5>
-              <div>Price</div>
-              <div>Stars</div>
-            </div>
-          </div>
-        </div>
-    </div>
+      <Carousel>
+        <Carousel.Item>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4" />
+              <Card.Body>
+                <Card.Text>Category</Card.Text>
+                <Card.Title>Product Name</Card.Title>
+                <Card.Text>Price</Card.Text>
+                <Card.Text>Stars</Card.Text>
+              </Card.Body>
+          </Card>
+        </Carousel.Item>
+      </Carousel>
     )
   }
 }
