d24ee72622b3e17c3844b7c990605244b333f460
Andrew Jyan <andrew.jyan4@gmail.com>
2020-03-24 17:27:13 -0400

    created base card in bootstrap

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -7,14 +7,21 @@ export default class RelatedProducts extends React.Component {
   }
 
   render() {
-    return (<div>
-      <div>
-        <img></img>
-        <div>Category</div>
-        <div>Product Name</div>
-        <div>Price</div>
-        <div>Stars</div>
-      </div>
-    </div>)
+    return (
+    <div>
+        <div class='container'>
+          <div class='col-sm-5'>
+            <div class="card text-left 2-50">
+            <img src='https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4'></img>
+            <div class="card-body"></div>
+              <div>Category</div>
+              <h5 class="card-title">Product Name</h5>
+              <div>Price</div>
+              <div>Stars</div>
+            </div>
+          </div>
+        </div>
+    </div>
+    )
   }
 }
