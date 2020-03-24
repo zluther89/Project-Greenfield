bb0410d37c4a145dd16f769a22f683ca2f37b9cc
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-24 17:23:59 -0400

    added announcement holder

--- src/components/Overview/Banner.jsx
+++ src/components/Overview/Banner.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Best Website Ever</h1>
+    <div>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-5">Best Website Ever</h1>
+        </div>
+      </div>
+      <div id="specialOffer">
+        <p>SITE WIDE ANNOUNCEMENT --- THIS IS THE GREATEST WEBSITE EVER</p>
       </div>
     </div>
   );
