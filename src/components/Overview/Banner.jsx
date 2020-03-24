8c364371d46588676d3807b3686a0def1cdb164d
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-24 19:51:07 -0400

    created base structure

--- src/components/Overview/Banner.jsx
+++ src/components/Overview/Banner.jsx
@@ -8,6 +8,7 @@ export default function Banner() {
           <h1 class="display-5">Best Website Ever</h1>
         </div>
       </div>
+
       <div id="specialOffer">
         <p>SITE WIDE ANNOUNCEMENT --- THIS IS THE GREATEST WEBSITE EVER</p>
       </div>
--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Banner from "./Banner";
+import ProductInfo from "./ProductInfo";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import StyleSelector from "./StyleSelector";
 
 export default class Overview extends React.Component {
   constructor(props) {
@@ -10,10 +13,25 @@ export default class Overview extends React.Component {
   render() {
     return (
       <div>
-        <div className="overview">
-          <Banner />
+        <div className="overviewFull">
+          <div class="overviewBanner">
+            {" "}
+            <Banner />
+          </div>
+          <div className="overviewMain container-fluid">
+            <div class="row overviewRow">
+              <div className="imageGallery col-xs-7 col-sm-7 col-md-7">
+                <ImageGallery />
+              </div>
+              <div className="productInfo col-xs-5 col-sm-5 col-md-5">
+                <StyleSelector />
+              </div>
+            </div>
+          </div>
+          <div className="overviewBottom">
+            <ProductInfo />
+          </div>
         </div>
-        <div id="overviewContent"></div>
       </div>
     );
   }
