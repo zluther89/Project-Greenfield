5126bc2db7d18ae5e40e87903fd4bc01e34be81e
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-24 20:14:46 -0400

    chnaged RR position

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./components/Redux/Store.js";
-import 'bulma/css/bulma.css'
+
 import "bootstrap/dist/css/bootstrap.css";
-import { Plus } from 'react-feather';
+
 import "bootstrap/dist/css/bootstrap.css";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
