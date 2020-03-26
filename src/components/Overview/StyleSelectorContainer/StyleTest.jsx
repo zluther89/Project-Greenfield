31051ba6e7bbcd7f07cc7bd5f04407f4258ed839
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-25 20:49:34 -0400

    more work on select syle

---
+++ src/components/Overview/StyleSelectorContainer/StyleTest.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default class StyleTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="circlePhotoBox">
+        <div id="photos left">
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder "
+              class="rounded-circle"
+            />
+          </div>
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder"
+              class="rounded-circle"
+            />
+          </div>
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder"
+              class="rounded-circle"
+            />
+          </div>
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder"
+              class="rounded-circle"
+            />
+          </div>
+        </div>
+        <div id="photos right">
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder"
+              class="rounded-circle"
+            />
+          </div>
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder "
+              class="rounded-circle"
+            />
+          </div>
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder "
+              class="rounded-circle"
+            />
+          </div>
+          <div class="logo-image">
+            <img
+              src="https://avatars1.githubusercontent.com/u/12416599?s=460&u=dd647676df3df2357c7aa8045c1a5e14fbcec5ac&v=4"
+              alt="Placeholder "
+              class="rounded-circle"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
