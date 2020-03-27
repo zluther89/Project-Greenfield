6af163cd2daa016bafd87858fbdfeed0dfa614c9
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-27 14:38:08 -0400

    size and quanity select working

--- src/components/Overview/StyleSelectorContainer/SizeQuantitySelect.jsx
+++ src/components/Overview/StyleSelectorContainer/SizeQuantitySelect.jsx
@@ -6,13 +6,19 @@ import { ThumbsDown } from "react-feather";
 export default class SizeQuantitySelect extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { sizesClicked: false };
+    this.state = {
+      sizesClicked: false,
+      sizeSelected: null,
+      sizeAvailable: null,
+      quantitySelected: null
+    };
     this.populateSizes = this.populateSizes.bind(this);
+    this.setQuantity = this.setQuantity.bind(this);
+    // this.populateQuantity = this.populateQuantity.bind(this);
   }
 
   populateSizes() {
     let index = this.props.currentStyle;
-
     let sizes = Object.keys(this.props.styleData.results[index].skus);
     let amount = Object.values(this.props.styleData.results[index].skus);
     console.log(sizes, amount, this.props.styleData.results[index]);
@@ -20,12 +26,24 @@ export default class SizeQuantitySelect extends React.Component {
       this.props.styleData.results &&
       sizes.map((size, index) => {
         return (
-          <Dropdown.Item href="#/action-1">
+          <Dropdown.Item
+            href="#/action-1"
+            onClick={() => {
+              this.setState({
+                sizeSelected: size,
+                sizeAvailable: amount[index]
+              });
+            }}
+          >
             {size} -- {amount[index]}
           </Dropdown.Item>
         );
       })
     );
+  }
+
+  setQuantity(num) {
+    this.setState({ quantitySelected: num });
   }
 
   render() {
@@ -34,7 +52,10 @@ export default class SizeQuantitySelect extends React.Component {
         <div className="row">
           {" "}
           <div class="col-xs-7 col-sm-7 col-md-7">
-            <DropdownButton id="dropdown-basic-button-size" title="SELECT SIZE">
+            <DropdownButton
+              id="dropdown-basic-button-size"
+              title={this.state.sizeSelected || "SELECT SIZE"}
+            >
               {this.props.styleData &&
                 this.props.styleClicked &&
                 this.populateSizes()}
@@ -44,13 +65,43 @@ export default class SizeQuantitySelect extends React.Component {
             {" "}
             <DropdownButton
               id="dropdown-basic-button-quantity"
-              title="SELECT QUANTITY"
+              title={this.state.quantitySelected || "SELECT QUANTITY"}
             >
-              <Dropdown.Item href="#/action-1">1</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">2</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">3</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">4</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">5</Dropdown.Item>
+              {this.state.sizeSelected && this.state.sizeAvailable && (
+                <div>
+                  {" "}
+                  <Dropdown.Item
+                    href="#/action-1"
+                    onClick={() => this.setQuantity(1)}
+                  >
+                    1
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="#/action-2"
+                    onClick={() => this.setQuantity(2)}
+                  >
+                    2
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="#/action-3"
+                    onClick={() => this.setQuantity(3)}
+                  >
+                    3
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="#/action-3"
+                    onClick={() => this.setQuantity(4)}
+                  >
+                    4
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    href="#/action-3"
+                    onClick={() => this.setQuantity(5)}
+                  >
+                    5
+                  </Dropdown.Item>
+                </div>
+              )}
             </DropdownButton>
           </div>
         </div>
