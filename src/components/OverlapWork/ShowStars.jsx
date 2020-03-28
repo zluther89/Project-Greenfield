3d3035fa1eb8e3fb45adb58d438709dd342cd4b6
kqiu10 <qiukan1011@126.com>
2020-03-28 14:40:58 -0400

    to merge from master

--- src/components/OverlapWork/ShowStars.jsx
+++ src/components/OverlapWork/ShowStars.jsx
@@ -1,5 +1,24 @@
 import Axios from "axios";
 import React from "react"
+//help function
+const TranverStar = star => {
+  const Int = parseInt(star)
+  const modul = star - Int
+  let ratingPercen
+  if (modul >= 0 & modul < 0.125) {
+    ratingPercen = Int
+  } else if (modul >= 0.125 & modul < 0.375) {
+    ratingPercen = Int + 0.48
+  }else if (modul >= 0.375  & modul< 0.625) {
+    ratingPercen = Int + 0.6
+  }else if (  modul >= 0.625 & modul< 0.875) {
+    ratingPercen = Int + 0.7
+  }else {
+    ratingPercen = Int + 1
+  }
+  ratingPercen = 20 * ratingPercen
+  return ratingPercen
+}
 class ShowStars extends React.Component{
   constructor(props) {
     super(props)
@@ -13,7 +32,10 @@ class ShowStars extends React.Component{
   }
   GetReviewMet(product_id = 3) {
     let count = 0;
-    let sum =0
+    let sum = 0
+    if (this.props.star) {
+
+    }
     Axios.get(`http://3.134.102.30/reviews/${product_id}/meta`)
       .then(response => {
         const AllRatings = response.data.ratings
@@ -25,24 +47,10 @@ class ShowStars extends React.Component{
         //get the one round number
         const roundedRating = Math.round(rating * 10) / 10
         //get percentage
-        //first get the number whose denominator is ten
-        const NumberInTen = rating
+        //first get the number whose denominator is five
+        const ratingPercen = TranverStar(rating)
         //get int part
-        const Int = parseInt(NumberInTen)
-        const modul = NumberInTen - Int
-        let ratingPercen
-        if (modul >= 0 & modul < 0.125) {
-          ratingPercen = Int
-        } else if (modul >= 0.125 & modul < 0.375) {
-          ratingPercen = Int + 0.48
-        }else if (modul >= 0.375  & modul< 0.625) {
-          ratingPercen = Int + 0.6
-        }else if (  modul >= 0.625 & modul< 0.875) {
-          ratingPercen = Int + 0.7
-        }else {
-          ratingPercen = Int + 1
-        }
-        ratingPercen=20*ratingPercen
+
         this.setState({rating:roundedRating,ratingPercen:ratingPercen})
       }
       )
@@ -54,6 +62,7 @@ class ShowStars extends React.Component{
       <div className="progress border-0" style={{"height":"30"},{"width":"50%"}}>
         <div className="progress-bar progress-bar-striped progress-bar-animated  " style={{ "width": `${this.state.ratingPercen}%` }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" ><img style={{ "border": "hidden" }}className="grade-star-img "></img></div>
 </div>
+
     )
 
   }
--- src/components/RatingsReviews/individualReview.jsx
+++ src/components/RatingsReviews/individualReview.jsx
@@ -1,31 +1,48 @@
 import React from "react";
+import { Check } from "react-feather";
 
 const individualReview = ({ results }) => {
+  //review body function
   return results.map(result => {
+    //change date to Month DD, YYYY
+    var date = new Date(`${result.date}`);
+    const options = { month: "long", day: "numeric", year: "numeric" };
+    const NewDate = new Intl.DateTimeFormat("en-US", options).format(date);
     return (
-      <div
-        className="p-3 "
-        style={{ height: "50%", width: "100%" }}
-      >
+      <div className="p-3 "key={result.id} style={{ height: "50%", width: "100%" }}>
         <div className="row justify-content-between" style={{ height: "10%" }}>
-          <div className="col-4 ml-2">star bar</div>
-          <div className="col-4">username+date</div>
+          <div className="col-5 ml-2">star bar</div>
+          <div className="col RightText">
+            {result.reviewer_name}, {NewDate}
+          </div>
         </div>
-        <div className="row justify-content-between my-2 ml-2" style={{ height: "10%" }}>
-          <b>Review Summary</b>
+        <div className="row my-2 ml-2" style={{ height: "10%" }}>
+          <b>{result.summary.slice(0, 60)}</b>
         </div>
-        <div className="row my-2 ml-2" style={{ height: "20%" }}>
-          <p>Review Body</p>
+        <div className="row my-2 ml-2" style={{ height: "30%" }}>
+          <p>{result.body.slice(0, 250)}</p>
+          {result.body.length > 250 ? <u type="button">Show more</u> : null}
         </div>
-        <div className="row  ml-2" style={{ height: "10%" }}>
-          <p>recommend</p>
-        </div>
-        <div className="row  my-2 ml-2" style={{ height: "20%" }}>
-          <p>response</p>
-        </div>
-        <div className="row  my-1 ml-2" style={{ height: "5%" }}>
-          <p>helpful</p>
-
+        {result.recommend === 1 ? (
+          <div className="row  ml-2 my-4" style={{ height: "10%" }}>
+            <Check size={24} />
+            <p>I recommend this product</p>
+          </div>
+        ) : null}
+        {result.response ===true && result.response !== "" ? (
+          <div
+            className="row  my-2 ml-2 p-3 mb-2 bg-light"
+            style={{ height: "20%" }}
+          >
+            <b style={{ height: "5%" }}>Response:</b>
+            <p>{result.response}</p>
+          </div>
+        ) : null}
+        <div className="row  my-1 ml-2 my-4" style={{ height: "5%" }}>
+          <p className="RightMargin">Helpful?</p>
+          <u  type="button">Yes</u>
+          <p style={{"borderRight":"ridge","width":"7%"}}className="RightMargin">({result.helpfulness})</p>
+          <u className="RightMargin"type="button">Report</u>
         </div>
         <hr className="Hrsolid"></hr>
       </div>
