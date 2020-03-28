bb0dd8c81996a4427ad63341bda3dd052f2f553c
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-28 11:52:05 -0400

    added carousel notes

--- src/components/Overview/ImageGallery/Carousel.jsx
+++ src/components/Overview/ImageGallery/Carousel.jsx
@@ -39,3 +39,34 @@ export default class ImageCarousel extends React.Component {
     );
   }
 }
+
+{
+  /* <div class="row hidden-xs" id="slider-thumbs">
+<!-- Bottom switcher of slider -->
+<ul class="hide-bullets">
+    <li class="col-sm-2">
+        <a class="thumbnail" id="carousel-selector-0"><img src="http://placehold.it/170x100&amp;text=one"></a>
+    </li>
+
+    <li class="col-sm-2">
+        <a class="thumbnail" id="carousel-selector-1"><img src="http://placehold.it/170x100&amp;text=two"></a>
+    </li>
+
+    <li class="col-sm-2">
+        <a class="thumbnail" id="carousel-selector-2"><img src="http://placehold.it/170x100&amp;text=three"></a>
+    </li>
+
+    <li class="col-sm-2">
+        <a class="thumbnail" id="carousel-selector-3"><img src="http://placehold.it/170x100&amp;text=four"></a>
+    </li>
+
+    <li class="col-sm-2">
+        <a class="thumbnail" id="carousel-selector-4"><img src="http://placehold.it/170x100&amp;text=five"></a>
+    </li>
+
+    <li class="col-sm-2">
+        <a class="thumbnail" id="carousel-selector-5"><img src="http://placehold.it/170x100&amp;text=six"></a>
+    </li>
+</ul>                 
+</div> */
+}
