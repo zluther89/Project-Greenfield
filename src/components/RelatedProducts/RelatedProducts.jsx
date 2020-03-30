2afa407ed7c0f3e5b865309907d6450ba483b9e7
Andrew Jyan <andrew.jyan4@gmail.com>
2020-03-30 18:06:50 -0400

    added your outfit functionality, need to fix add to outfit

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 // React Component imports
 import ProductCard from "./ProductCard";
 import ComparisonModal from './ComparisonModal'
+import OutfitCard from './OutfitCard'
 
 // Bootstrap imports
 import CardDeck from "react-bootstrap/CardDeck";
@@ -34,18 +35,20 @@ class RelatedProducts extends React.Component {
       relatedProducts: [],
       productInfo: {},
       outfitId: [],
+      outfitNames: [],
       outfitInfo:{},
+      outfitLoaded: false,
       clickedProduct: null,
       showModal: false
     }; 
     this.handleCompare = this.handleCompare.bind(this)
     this.handleClick = this.handleClick.bind(this)
     this.handleAddToOutfit = this.handleAddToOutfit.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   handleClick(e) {
     let id = e.currentTarget.className.split(' ')[1]
-    console.log('i got the id!', id)
     // should update store with the clicked e target value id
     this.props.getNewProductThunk(id)
   }
@@ -96,22 +99,43 @@ class RelatedProducts extends React.Component {
     }
     this.setState({outfitId: currentOutfit}, () => {
       localStorage.setItem('outfit', JSON.stringify(currentOutfit))
-
     })
+    this.getOutfits()
   }
 
   getOutfits(){
     // retrieves the favorites from the local storage
     let outfitId = JSON.parse(localStorage.getItem('outfit'))
     if (!!outfitId){
-      this.setState({outfitId: outfitId}) 
-    } else {
-      this.setState({outfitId: []})
+      this.setState({outfitId: outfitId}, () => {
+        let outfitNames = []
+        let outfitInfo = {}
+        async function getData() {
+          for (let id of outfitId) {
+          await axios.get(`http://3.134.102.30/products/${id}`).then( ({data}) => {
+            outfitNames.push(data)
+          } )
+          await axios.get(`http://3.134.102.30/products/${id}/styles`).then( ({data}) => {
+            outfitInfo[data.product_id] = data.results
+          })
+        }}
+        getData().then( () => {
+          this.setState({outfitNames: outfitNames})
+          this.setState({outfitInfo: outfitInfo})
+          this.setState({outfitLoaded: true})
+        }
+        )
+      }) 
     }
+  }
 
-    // localStorage.clear();
-    // pulls card info necessary for the individual favorites
-    // sets state of the favorites array
+  handleDelete(e) {
+    let id = Number(e.target.value)
+    let storage = this.state.outfitId.slice()
+    let index = storage.indexOf(id)
+    storage.splice(index, 1)
+    localStorage.setItem('outfit', JSON.stringify(storage))
+    this.getOutfits();
   }
 
   render() {
@@ -162,18 +186,20 @@ class RelatedProducts extends React.Component {
               </Button>{' '}
             </Card>
             {/* map out the rest of the favorites after pulling favorites */}
-            {this.state.relatedProducts.map( (product,i) => {      
-              return (
-                    <ProductCard 
-                      key={i}
-                      index={i}
-                      handleClick={this.handleClick}
-                      handleCompare={this.handleCompare}
-                      productInfo={this.state.productInfo} 
-                      product={product}
-                    /> 
-              
-              )
+            {this.state.outfitId.map( (outfitId,i) => { 
+              if(this.state.outfitLoaded){
+                return (
+                      <OutfitCard 
+                        key={i}
+                        index={i}
+                        outfitId={outfitId}
+                        outfitNames={this.state.outfitNames}
+                        outfitInfo={this.state.outfitInfo}
+                        handleDelete={this.handleDelete}
+                      /> 
+                
+                )
+              }     
             })}
           </CardDeck>
         <br></br>
