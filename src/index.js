758d4b13ded4ff8a56d97bbae770830ca6eaa762
Andrew Jyan <andrew.jyan4@gmail.com>
2020-03-31 22:52:56 -0400

    fixed flex box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import AppRouter from './Router.js';
 import { Provider } from 'react-redux';
 import store from './components/Redux/Store.js';
