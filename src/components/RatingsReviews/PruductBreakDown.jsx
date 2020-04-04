0a0247525c31eba8a9fcb3b57558a3226aa36b50
kqiu10 <qiukan1011@126.com>
2020-04-04 15:00:18 -0400

    change all image from imgurl

--- src/components/RatingsReviews/PruductBreakDown.jsx
+++ src/components/RatingsReviews/PruductBreakDown.jsx
@@ -3,29 +3,30 @@ const ProductBreakdown = ({ characteristics }) => {
   return characteristics.map(characteristic => {
     if (characteristic[0] === "Quality") {
       return (
-        <div key={characteristic[1].id} className="container">
+ <div key={characteristic[1].id} className="container">
           {/* title */}
           <div className="row align-items-start">{characteristic[0]}</div>
           {/* progress bar */}
           <div className="row align-items-center">
             <div className="col">
             <div className="productslidercontainer">
-            <input type="range" min="1" max="100" defaultValue={`${characteristic[1].value*20}%`} className="productslider"></input>
+            <input type="range" min="1" max="100" defaultValue={`${characteristic[1].value*20}%`} className="ProductSlider"></input>
 </div>
-              <div className="progress border-0  grade-product-bar " >
-        <div className="progress-bar "role="progressbar" aria-valuemin="0" aria-valuemax="100" ><div className="grade-product-img" ></div></div>
+            <div className="progress border-0  grade-product-bar " >
+        <div className="progress-bar "  role="progressbar" aria-valuemin="0" aria-valuemax="100" ><div className="grade-product-img" ></div></div>
 </div>
             </div>
           </div>
+
           <div className="row align-items-end">
             <div className="col">
-              <p>poor</p>
+              <p>Poor</p>
             </div>
-            <div className="col">
+            <div className="col CenterText">
               <p></p>
             </div>
             <div className="col RightText">
-              <p>great</p>
+              <p>Great</p>
             </div>
           </div>
         </div>
